19184c835ccc77104b7450d1a45484f99dd51c1d
thuankhang <matthuanphong@gmail.com>
2019-08-01 11:47:44 +0700

    Add lesson 5

---
+++ lesson5khang/src/main/java/com/trainingdeveloperpro/androidk001/lesson5khang/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.trainingdeveloperpro.androidk001.lesson5khang
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
